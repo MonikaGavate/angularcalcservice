c17999baa4bfb08be52ec1ecaa0f39d11c62bbf7
git config --global user.email gavtemonika@gmail.com <gavtemonika@gmail.com>
2022-05-08 14:09:14 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalcService } from './calc.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcalcservice';
+  sum:number=0;
+  constructor(calc:CalcService){
+    this.sum=calc.add(1,2,3,4,5);
+  }
 }
